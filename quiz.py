bc0700c9b5c496587602c0fc143d202179ad6502
JTEZZY05 <tezohtebong@gmail.com>
2025-09-13 06:47:04 -0600

    Set up the quiz function

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
-questions_answers = {"What is 4 + 4?":8,
+questions_answers = {"What is 4 + 4? ":8,
                      "What is 2 x 2?":4,
-                     "What is 4 \u00F7 2?":2,
+                     "What is 4 ÷ 2?":2,
                      "What is 10 - 5?":5,
                      "What is the capital of France?":"Paris",
                      "What is the color of grass?":"green",
@@ -8,3 +8,14 @@ questions_answers = {"What is 4 + 4?":8,
                      "Is bread a food?":"yes",
                      "Who was the first man to throw a pie?":"Harold Lloyd",
                      "Who is the coolest programmer(Hint:It's JTEZZY05:)":"JTEZZY05"}
+
+def quiz_run(questions_answers):
+    print("🍁 Welcome to my quiz game! 🍁")
+    print("Please answer the following questions! ")
+
+    score = 0
+    wrong_questions = []
+    total_questions = len(questions_answers)
+
+    current_question = 1
+    
