625bf77c4fc79461f94228f914975ae16023713a
JTEZZY05 <tezohtebong@gmail.com>
2025-09-13 07:09:04 -0600

    Set up the for loop to loop through the questions, added comments and included a check to ensure that str inputs are converted to integers when needed

--- quiz.py
+++ quiz.py
@@ -1,3 +1,4 @@
+#Dictionary for questions and their respective answers
 questions_answers = {"What is 4 + 4? ":8,
                      "What is 2 x 2?":4,
                      "What is 4 ÷ 2?":2,
@@ -8,14 +9,22 @@ questions_answers = {"What is 4 + 4? ":8,
                      "Is bread a food?":"yes",
                      "Who was the first man to throw a pie?":"Harold Lloyd",
                      "Who is the coolest programmer(Hint:It's JTEZZY05:)":"JTEZZY05"}
-
+#Function to ask the questions and check users answers against the correct ones 
 def quiz_run(questions_answers):
     print("🍁 Welcome to my quiz game! 🍁")
     print("Please answer the following questions! ")
 
-    score = 0
-    wrong_questions = []
-    total_questions = len(questions_answers)
+    score = 0 #Score tracker
+    wrong_questions = [] # Tracks what numbers were wrong
+    total_questions = len(questions_answers) 
 
     current_question = 1
-    
+    #For loop that loops through the dictionary 
+    for questions, answers in questions_answers():
+        print(f"Question {current_question}/{total_questions}")
+        user_input = input("Question: "+questions)
+        #This checks if the current answer is an integer, in which case it converts users string input to an integer
+        if isinstance(answers,int):
+            if user_input.isdigit():
+                user_input = int(user_input)
+        
