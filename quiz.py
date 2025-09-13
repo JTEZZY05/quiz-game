95559a83bf8238edcbeb98cbb9622fe67e656df5
JTEZZY05 <tezohtebong@gmail.com>
2025-09-13 07:54:55 -0600

    Fix a bug that occured...It kept saying you're gosh darn right after each question. This is because I was comparing the length of the question to 10, which are the same. Instead I set it to compare the *CURRENT* question to question 10. I also added some safety guards to ensure that user inputs are properly parsed. I THINK WE'RE DONEZO!!!!!!

--- quiz.py
+++ quiz.py
@@ -1,14 +1,14 @@
 #Dictionary for questions and their respective answers
 questions_answers = {"What is 4 + 4? ":8,
-                     "What is 2 x 2?":4,
-                     "What is 4 ÷ 2?":2,
-                     "What is 10 - 5?":5,
-                     "What is the capital of France?":"Paris",
-                     "What is the color of grass?":"green",
-                     "Is water wet?":"yes",
-                     "Is bread a food?":"yes",
-                     "Who was the first man to throw a pie?":"Harold Lloyd",
-                     "Who is the coolest programmer(Hint:It's JTEZZY05:)":"JTEZZY05"}
+                     "What is 2 x 2? ":4,
+                     "What is 4 ÷ 2? ":2,
+                     "What is 10 - 5? ":5,
+                     "What is the capital of France? ":"Paris",
+                     "What is the color of grass? ":"Green",
+                     "Is water wet? ":"Yes",
+                     "Is bread a food? ":"Yes",
+                     "Who was the first man to throw a pie? ":"Harold Lloyd",
+                     "Who is the coolest programmer(Hint:It's JTEZZY05:) ":"JTEZZY05"}
 #Function to ask the questions and check users answers against the correct ones 
 def quiz_run(questions_answers):
     print("🍁 Welcome to my quiz game! 🍁")
@@ -27,30 +27,36 @@ def quiz_run(questions_answers):
         if isinstance(answers,int):
             if user_input.isdigit():
                 user_input = int(user_input)
-        if user_input == answers and total_questions < 9: # Loops up untill 8, this is because 9 and 10 have special responses
+        #Case to ensure that user input is correctly capitalized on questions 5 and 6(First letter only)
+        if current_question == 5 or current_question == 6 or current_question == 7 or current_question == 8:
+            user_input = user_input.capitalize()
+        #Case to ensure that user input is correctly capitalized on question 9(First letters of both words)
+        if current_question == 9:
+            user_input = user_input.title()
+        if user_input == answers and current_question < 9: # Loops up untill 8, this is because 9 and 10 have special responses
             print("✨WOWZERS YOU ARE CORRECT✨")
             score+=1
         elif user_input != answers:
             print("❌YOU ARE WRONG❌")
             wrong_questions.append(current_question) #Updates the wrong question tracker for each question missed
-        elif user_input == answers and total_questions == 9:
+        elif user_input == answers and current_question == 9:
             print("✨🔥🚀HOW DO YOU EVEN KNOW THAT?✨🔥🚀") #I am gonna be impressed if someone just knows this
             score+=1
-        elif user_input == answers and total_questions ==10:
+        elif user_input == answers and current_question ==10:
             print("😎💯AND YOU'RE GOSH DARN RIGHT😎💯") #Well then, that settles things
             score+=1
         current_question+=1
         #Final summary
-        print("You finished the quiz 🎉")
-        print(f"Your final score is {score}/{total_questions}")
+    print("You finished the quiz 🎉")
+    print(f"Your final score is {score}/{total_questions}")
         
-        if score == total_questions:
-            print("🎉✨PERFECT SCORE🎉✨")
-        elif score == 0:
-            print("0_0 HOWWWWWWWWWWWWWWWWWWWW")
-            print("You got.....EVERYTHING WRONG OMLLLL")
-        else:
-            print("You missed these questions:",wrong_questions)
+    if score == total_questions:
+        print("🎉✨PERFECT SCORE🎉✨")
+    elif score == 0:
+        print("0_0 HOWWWWWWWWWWWWWWWWWWWW")
+        print("You got.....EVERYTHING WRONG OMLLLL")
+    else:
+        print("You missed these questions:",wrong_questions)
 quiz_run(questions_answers)
 
 
